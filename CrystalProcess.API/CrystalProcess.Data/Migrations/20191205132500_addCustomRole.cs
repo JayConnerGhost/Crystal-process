c7f9057203563604b55ec3c6f58f6393b44bcc12
jay katie martin <jaykatiemartin@outlook.com>
2019-12-05 14:48:50 +0000

    adding LoggedIn framework

---
+++ CrystalProcess.API/CrystalProcess.Data/Migrations/20191205132500_addCustomRole.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace CrystalProcess.Data.Migrations
+{
+    public partial class addCustomRole : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "CustomRole",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(nullable: true),
+                    UserId = table.Column<int>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_CustomRole", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_CustomRole_Users_UserId",
+                        column: x => x.UserId,
+                        principalTable: "Users",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_CustomRole_UserId",
+                table: "CustomRole",
+                column: "UserId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "CustomRole");
+        }
+    }
+}
